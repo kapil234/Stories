d7db9eff066ac0f50af0c86746ed4a56a7049868
kapil234 <109575291+kapil234@users.noreply.github.com>
2026-05-09 13:45:46 +0530

    fix auth controller

--- Backend/middleware/auth.js
+++ Backend/middleware/auth.js
@@ -1,45 +1,47 @@
-const jwt=require("jsonwebtoken");
-const User=require("../models/userModel")
-
-
+const jwt = require("jsonwebtoken");
+const User = require("../models/userModel");
 
 async function auth(req, res, next) {
   try {
-  
+
     const token = req.cookies?.token;
 
     if (!token) {
       return res.status(401).json({
         message: "Please login first",
-        error: true,
         success: false,
+        error: true,
       });
     }
-    const decoded = jwt.verify(token, process.env.TOKEN_SECRET_KEY);
-  
-      req.userId = decoded._id;
 
-    const user = await User.findById(decoded._id)
+    const decoded = jwt.verify(
+      token,
+      process.env.TOKEN_SECRET_KEY
+    );
+
+    const user = await User.findById(decoded._id);
+
     if (!user) {
       return res.status(401).json({
         message: "User not found",
         success: false,
+        error: true,
       });
     }
 
     req.user = user;
-   
+    req.userId = user._id;
+
     next();
 
   } catch (err) {
+
     return res.status(401).json({
       message: "Invalid or expired token",
-      error: true,
       success: false,
+      error: true,
     });
   }
 }
-
-
 
 module.exports = auth;
