f4be1587879a64ba8f9b3ec35bca8182f7ce1393
kapil234 <109575291+kapil234@users.noreply.github.com>
2026-05-09 13:44:03 +0530

    update toggle bookmark

--- Backend/controllers/toggleBookmark.js
+++ Backend/controllers/toggleBookmark.js
@@ -4,69 +4,49 @@ const User = require("../models/userModel");
 const Story = require("../models/StoryModel");
 
 async function toggleBookmark(req, res) {
-
   try {
-
     const storyId = req.params.id;
 
-    if (
-      !mongoose.Types.ObjectId.isValid(
-        storyId
-      )
-    ) {
+    if (!mongoose.Types.ObjectId.isValid(storyId)) {
       return res.status(400).json({
         success: false,
         message: "Invalid story id",
       });
     }
 
-    const story = await Story.findById(
-      storyId
-    );
+    const story = await Story.findById(storyId);
 
     if (!story) {
-
       return res.status(404).json({
         success: false,
         message: "Story not found",
       });
     }
 
-    const user = await User.findById(
-      req.user._id
-    );
+    const user = await User.findById(req.user._id);
 
     if (!user) {
-
       return res.status(404).json({
         success: false,
         message: "User not found",
       });
     }
 
-    // IMPORTANT FIX
     if (!user.bookmarks) {
-
       user.bookmarks = [];
     }
 
     const alreadyBookmarked =
       user.bookmarks.some(
-        (id) =>
-          id.toString() === storyId
+        (id) => id.toString() === storyId
       );
 
     if (alreadyBookmarked) {
-
-      user.bookmarks =
-        user.bookmarks.filter(
-          (id) =>
-            id.toString() !== storyId
-        );
-
+      user.bookmarks = user.bookmarks.filter(
+        (id) => id.toString() !== storyId
+      );
     } else {
-
-      user.bookmarks.push(storyId);
+      user.bookmarks.push(story._id);
     }
 
     await user.save();
@@ -80,7 +60,6 @@ async function toggleBookmark(req, res) {
     });
 
   } catch (error) {
-
     console.log(error);
 
     res.status(500).json({
